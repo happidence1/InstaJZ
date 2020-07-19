bdcfcbb41cacf8786b54155952b41a072d15e784
happidence1 <happidence@gmail.com>
2020-07-18 23:46:16 -0500

    edit home page

--- instaJZ/urls.py
+++ instaJZ/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('insta/', include('Insta.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/signup', SignUp.as_view(), name = 'signup'),
+    path('', SignUp.as_view(), name = 'signup'),
 ]
